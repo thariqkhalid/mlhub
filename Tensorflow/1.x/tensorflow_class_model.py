e9dbb93b73d92fcc590647abb51bb71e6308d34e
Thariq Khalid <thariq.khalid@gmail.com>
2019-06-06 05:43:27 +0300

    Update tensorflow_class_model.py
    Implemented identity block and conv block

--- Tensorflow/1.x/tensorflow_class_model.py
+++ Tensorflow/1.x/tensorflow_class_model.py
@@ -15,19 +15,57 @@ def variable_summaries(var):
       tf.summary.histogram('histogram', var)
 
 class ResNet(object):
-    def __init__(self, images, labels, num_classes):
+    def __init__(self, images, labels, num_classes, global_step, name, is_training):
+        # Create a useful list of private variables for training the network 
         self._images = images
         self._labels = labels
+        self._global_step = global_step
+        self._name = name
+        self._is_training = is_training
+        self._flops = 0
+        self._weights = 0
 
     def model_fn(self):
-        self.
-    
-    def __resnet_block_3(self, input, channels, filter_size, momentum = 0.99, is_training=true):
-        out = tf.layers.conv2d(input, channels, filter_size)
-        out = tf.layers.batch_normalization(out, momentum=momentum, training=is_training)
-        out = tf.nn.relu(out)
-        out = tf.layers.conv2d(out, channels, filter_size)
-        out = tf.layers.batch_normalization(out, momentum=momentum, training=is_training)
-        out = tf.add(out,input)
-        out = tf.nn.relu(out)
+        filters = [64, 64, 128, 256, 512]
+        kernels = [7, 3, 3, 3, 3]
+        strides = [2, 1, 1, 1, 1]
 
+        # The first conv block
+        with tf.variable_scope("conv1"):
+            out = tf.layers.conv2d(self._images, filters[0], strides[0])
+            out = tf.layers.batch_normalization(out)
+            out = tf.nn.relu(out)
+            out = tf.nn.max_pool(out, 3, strides[0], padding="valid")
+        
+
+    
+    def __identity_block_3(self, input, channels, filter_size, strides, name="res_identity_block", momentum = 0.99, is_training=true):
+        with tf.variable_scope(name):
+            shortcut = input
+            out = tf.layers.conv2d(input, channels, filter_size, strides=1, padding="same")
+            out = tf.layers.batch_normalization(out, momentum=momentum, training=is_training)
+            out = tf.nn.relu(out)
+            out = tf.layers.conv2d(out, channels, filter_size, strides=1, padding="same")
+            out = tf.layers.batch_normalization(out, momentum=momentum, training=is_training)
+            out = tf.add(out,shortcut)
+            out = tf.nn.relu(out)
+
+            return out
+
+    def __conv_block(self, input, channels, filter_size, strides, name="res_conv_block", momentum = 0.99, is_training=true):
+        with tf.variable_scope(name):
+            shortcut = tf.layers.conv2d(input, channels, filter_size, strides, padding="same")
+            shortcut = tf.layers.batch_normalization(shortcut, momentum=momentum, training=is_training)
+
+            out = tf.layers.conv2d(input, channels, filter_size, strides=2, padding="same")
+            out = tf.layers.batch_normalization(out, momentum=momentum, training=is_training)
+            out = tf.nn.relu(out)
+            out = tf.layers.conv2d(out, channels, filter_size, strides=1, padding="same")
+            out = tf.layers.batch_normalization(out, momentum=momentum, training=is_training)
+            out = tf.nn.relu(out)
+            out = tf.layers.conv2d(out, channels, filter_size, strides=1, padding="same")
+            out = tf.layers.batch_normalization(out, momentum=momentum, training=is_training)
+            out = tf.add(out,shortcut)
+            out = tf.nn.relu(out)
+
+            return out
